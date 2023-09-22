4b7471f77e7963f837a05b68c243389dad8b15d2
AsanoGenki <asanogenki0218@icloud.com>
2023-09-23 08:36:56 +0900

    初期コミット

---
+++ AppBlockTimer/AppBlockTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBlockTimerApp.swift
+//  AppBlockTimer
+//
+//  Created by Genki on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AppBlockTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# AppBlockTimer
