058ef680d0c9ca05bfc5b5bb919079ae403215e4
AsanoGenki <asanogenki0218@icloud.com>
2023-09-23 14:38:11 +0900

    add: Int型を"00:00"のString型に変えるメソッドを追加

---
+++ AppBlockTimer/Utility.swift
@@ -0,0 +1,27 @@
+//
+//  Utility.swift
+//  AppBlockTimer
+//
+//  Created by Genki on 9/23/23.
+//
+
+import Foundation
+
+class Utility {
+    
+    static func intToTimeLabel(_ result: Int) -> String {
+        let seconds = String(result % 60)
+        let minutes = String((result / 60) % 60)
+        let hours = String(result / 3600)
+        let minuteStamp = minutes.count > 1 ? minutes : "0" + minutes
+        let hoursStamp = hours.count > 1 ? hours : "0" + hours
+        let secondsStamp = String(seconds.count > 1 ? seconds : "0" + seconds)
+        
+        if result / 3600 < 1 {
+            return "\(minuteStamp) : \(secondsStamp)"
+        } else {
+            return "\(hoursStamp) : \(minuteStamp) : \(secondsStamp)"
+        }
+    }
+    
+}
