d68529b108861a10ca7e62ae95314bac91d82aad
AsanoGenki <asanogenki0218@icloud.com>
2023-09-24 11:12:08 +0900

    add: スクリーンタイムの認証を追加

--- AppBlockTimer/AppBlockTimerApp.swift
+++ AppBlockTimer/AppBlockTimerApp.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 @main
 struct AppBlockTimerApp: App {
+    @ObservedObject var center = AuthorizationCenter.shared
     @AppStorage("setMinute") var setMinute = 0
     
     var body: some Scene {
         WindowGroup {
             TimerView(timerViewModel: TimerViewModel(model: TimerModel(minutes: setMinute)))
+                .onAppear {
+                    gotoRequestAuthorization()
+                }
         }
     }
+    
+    //スクリーンタイムを認証
+    func gotoRequestAuthorization() {
+        Task {
+            do {
+                try await center.requestAuthorization(for: .individual)
+            } catch {
+                
+            }
+        }
+    }
+    
 }
