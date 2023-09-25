b7fbeb5d645450a5a60e959898249085759d22d2
AsanoGenki <asanogenki0218@icloud.com>
2023-09-25 18:57:30 +0900

    upgrade: ユーザーがスクリーンタイムを認証していない場合、認証画面に遷移する

--- AppBlockTimer/AppBlockTimerApp.swift
+++ AppBlockTimer/AppBlockTimerApp.swift
@@ -11,26 +11,19 @@ import FamilyControls
 @main
 struct AppBlockTimerApp: App {
     @ObservedObject var center = AuthorizationCenter.shared
+    
     @AppStorage("setMinute") var setMinute = 0
+    @AppStorage("authority") var authority = false
+    
     
     var body: some Scene {
         WindowGroup {
+            
             TimerView(timerViewModel: TimerViewModel(model: TimerModel(minutes: setMinute)))
-                .onAppear {
-                    gotoRequestAuthorization()
+            
+                .onReceive(center.$authorizationStatus) { status in
+                    authority = status == .approved
                 }
         }
     }
-    
-    //スクリーンタイムを認証
-    func gotoRequestAuthorization() {
-        Task {
-            do {
-                try await center.requestAuthorization(for: .individual)
-            } catch {
-                
-            }
-        }
-    }
-    
 }
--- AppBlockTimer/Views/TimerView.swift
+++ AppBlockTimer/Views/TimerView.swift
@@ -9,33 +9,38 @@ import SwiftUI
 
 struct TimerView: View {
     @ObservedObject var timerViewModel: TimerViewModel
-    @AppStorage("setMinute") var setMinute = 0
+    @ObservedObject var appBlockViewModel = AppBlockViewModel()
+    @AppStorage("authority") var authority = false
     
     @State var minute = 15 //タイマーの最小時間が15分
     @State var offset: CGFloat = 0
     
     var body: some View {
-        NavigationView{
-            ZStack {
-                ZStack(alignment: .center){
-                    TimeCircleView(timerViewModel: timerViewModel, result: $timerViewModel.result)
+        if (authority) {
+            NavigationView{
+                ZStack {
+                    ZStack(alignment: .center){
+                        TimeCircleView(timerViewModel: timerViewModel, result: $timerViewModel.result)
+                        
+                        TimerLabelView(offset: $offset, result: $timerViewModel.result)
+                    }
+                    .frame(minHeight: 330)
+                    .padding(24)
+                    .padding(.top, -100)
                     
-                    TimerLabelView(offset: $offset, result: $timerViewModel.result)
+                    VStack(spacing: 12) {
+                        Spacer()
+                        TimeSliderView(offset: $offset, minute: $minute)
+                            .opacity(timerViewModel.result == 0 ? 1 : 0)
+                        
+                        BottomButtonView(timerViewModel: timerViewModel, appBlockViewModel: AppBlockViewModel(), result: $timerViewModel.result, minute: $minute, offset: $offset)
+                        
+                    }
                 }
-                .frame(minHeight: 330)
-                .padding(24)
-                .padding(.top, -100)
-                
-                VStack(spacing: 12) {
-                    Spacer()
-                    TimeSliderView(offset: $offset, minute: $minute)
-                        .opacity(timerViewModel.result == 0 ? 1 : 0)
-                    
-                    BottomButtonView(timerViewModel: timerViewModel, appBlockViewModel: AppBlockViewModel(), result: $timerViewModel.result, minute: $minute, offset: $offset)
-                    
-                }
+                .padding(.bottom, 40)
             }
-            .padding(.bottom, 40)
+        } else {
+            AuthorizationView()
         }
     }
 }
