4a0f9da29dc9b844331506b7cfaa4a95b971a340
AsanoGenki <asanogenki0218@icloud.com>
2023-09-26 08:56:00 +0900

    upgrade: カスタムシールドの作成

--- ShieldConfigurationExtension/ShieldConfigurationExtension.swift
+++ ShieldConfigurationExtension/ShieldConfigurationExtension.swift
@@ -1,6 +1,6 @@
 //
 //  ShieldConfigurationExtension.swift
-//  ShieldConfig
+//  ShieldConfigurationExtension
 //
 //  Created by Genki on 9/25/23.
 //
@@ -9,13 +9,17 @@ import ManagedSettings
 import ManagedSettingsUI
 import UIKit
 
-// Override the functions below to customize the shields used in various situations.
-// The system provides a default appearance for any methods that your subclass doesn't override.
-// Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
+fileprivate let imageName = "shield_image"
 class ShieldConfigurationExtension: ShieldConfigurationDataSource {
     override func configuration(shielding application: Application) -> ShieldConfiguration {
         // Customize the shield as needed for applications.
-        ShieldConfiguration()
+
+        return ShieldConfiguration(
+            backgroundColor: .white,
+            icon: UIImage(named: imageName),
+            title: ShieldConfiguration.Label(text: String(localized: "Blocked by\nAppBlockTimer"), color: .black),
+            primaryButtonLabel: ShieldConfiguration.Label(text: String(localized: "Dismiss"), color: .white)
+        )
     }
     
     override func configuration(shielding application: Application, in category: ActivityCategory) -> ShieldConfiguration {
